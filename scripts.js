07fa67da524c89f9b771755891eee3ff975ee7d4
uditandrebecca <138304276+uditandrebecca@users.noreply.github.com>
2023-07-01 18:33:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,206 @@
+let playIcon = $('#play-icon');
+let pauseIcon = $('#pause-icon');
+let player = $('#player');
+let mediaType = $('#media-type');
+let nowPlaying = $('#now-playing');
+let controls = $('#media-controls');
+let currentMediaType = "song";
+
+let currentSongIndex = 0;
+let currentFunnyIndex = 0;
+let playing = false;
+
+let songs = ["Portishead - Undenied.mp3",
+	"Wilco - Jesus, Etc..mp3",
+	"Weezer - Jacked Up.mp3",
+	"Wilco - Handshake Drugs.mp3",
+	"Uncle Tupelo - New Madrid.mp3",
+	"Weezer - Zombie Bastards.mp3",
+	"Black Country, New Road - Across The Pond Friend.mp3",
+	"Wilco - California Stars.mp3",
+	"Marina - Teen Idle.mp3",
+	"Golden Smog - Pecan Pie.mp3",
+	"Wilco - I-'ll Fight.mp3",
+	"Moby - Natural Blues.mp3",
+	"Wilco - I-'m the Man Who Loves You.mp3",
+	"Beirut - Scenic World.mp3",
+	"Wilco - Reservations.mp3",
+	"Marina - Froot.mp3"];
+
+let funnies = ["You mean the world to me - you're the girl of my dreams.",
+	"Let's say, hypothetically, that we were married. It's fair to assume that in this case, we would be husband and wife. Now would it also be reasonable to assume that I will keep you happy until the end of time? Answer that, liberal.",
+	"You're my comfort. Like a background of tender blue. Rain-soaked alleyways. Dandelions losing their heads.",
+	"I love you - my spirit dove, I would fold mountains for you.",
+	"Tragedy is guaranteed, but I would like you to know that my love for you grows stronger every day.",
+	"I'm quite a piece of work, so thank you for dealing with me. (You're no walk in the park either. Ha.)",
+	"I dream of distant lands, with frozen ladders that climb through bright windows. Vast pools wait beside silent lawns, where the sun halos in a circular summer. And I think about your kiss, my infinite lover, away from an ancient blaze.",
+	"The sun is waiting by the windowsill, and my fingertips write my will on your skin. I'm in love."]
+
+$(document).ready(() => {
+	console.log('Page loaded, magic ensues');
+	player.attr('src', "music/" + songs[currentSongIndex]);
+	nowPlaying.text(cleanName(songs[currentSongIndex]));
+	playing = false;
+})
+
+function handleBold(el) {
+	$(".media-select .button").removeClass('selected')
+	$(el).addClass('selected');
+}
+
+
+function playSong(el) {
+	currentMediaType = 'song'
+	console.log("Rebecca wants to play a song");
+	handleBold(el)
+	mediaType.text('Song Mode')
+	nowPlaying.text(cleanName(songs[currentSongIndex]));
+	player[0].pause()
+	player.attr('src', "music/" + songs[currentSongIndex]);
+	pausePlayer();
+	controls.show();
+}
+
+function playFunnyTime(el) {
+	console.log("Rebecca wants to see a sweet message");
+	currentMediaType = "funny"
+	handleBold(el)
+	nowPlaying.text(cleanName(funnies[currentFunnyIndex]));
+	pausePlayer();
+	player.attr('src', "funnytimes/" + funnies[currentFunnyIndex]);
+	controls.show()
+}
+
+function freakout(el) {
+	console.log("Rebecca is freaking out");
+	handleBold(el)
+	mediaType.text('Freakout engaged, Udit is being notified. Deep breaths!')
+	player[0].pause()
+	nowPlaying.text('')
+	controls.hide()
+	sendPush();
+}
+
+function playPauseClicked(el) {
+	console.log("Rebecca pressed play/pause");
+	let clicked = $(el).find('img').not(".hidden")[0];
+	clicked = $(clicked).attr('id');
+	if (clicked === "play-icon") {
+		// Rebecca wants to play the media
+		player[0].play();
+
+		if (currentMediaType === "song") {
+			nowPlaying.text(cleanName(songs[currentSongIndex]))
+		} else {
+			pauseIcon.removeClass('hidden');
+			playIcon.removeClass('hidden')
+		}
+		playing = true
+
+		playIcon.addClass('hidden');
+		pauseIcon.removeClass('hidden');
+
+	} else {
+		// Rebecca is pausing
+		player[0].pause();
+		playing = false;
+		playIcon.removeClass('hidden')
+		pauseIcon.addClass('hidden');
+	}
+}
+
+function nextClicked() {
+	console.log('Rebecca clicked next');
+	if (currentMediaType === "song") {
+		currentSongIndex++
+		currentSongIndex > songs.length ? currentSongIndex = 0 : true;
+		player.attr('src', "music/" + songs[currentSongIndex]);
+		nowPlaying.text(cleanName(songs[currentSongIndex]));
+	} else {
+		currentFunnyIndex++;
+		currentFunnyIndex > funnies.length ? currentFunnyIndex = 0 : true;
+		player.attr('src', "funnytimes/" + funnies[currentFunnyIndex]);
+		nowPlaying.text(cleanName(funnies[currentFunnyIndex]));
+	}
+
+	if (playing === true) {
+		player[0].play();
+	}
+}
+
+function prevClicked() {
+	console.log("Rebecca clicked previous")
+
+	if (currentMediaType === "song") {
+		currentSongIndex--;
+		currentSongIndex < 0 ? currentSongIndex = 0 : true;
+		player.attr('src', "music/" + songs[currentSongIndex]);
+		nowPlaying.text(cleanName(songs[currentSongIndex]));
+	} else {
+		currentFunnyIndex--;
+		currentFunnyIndex < 0 ? currentFunnyIndex = 0 : true;
+		player.attr('src', "funnytimes/" + funnies[currentFunnyIndex]);
+		nowPlaying.text(cleanName(funnies[currentFunnyIndex]));
+	}
+
+	if (playing === true) {
+		player[0].play();
+	}
+}
+
+
+player.on("ended", () => {
+	console.log("Player has ended, hide play button, show pause");
+	pausePlayer();
+})
+
+function pausePlayer() {
+	player[0].pause()
+	pauseIcon.addClass('hidden');
+	playIcon.removeClass('hidden');
+	playing = false;
+}
+
+
+function sendPush() {
+	let push = {
+		"active": "true",
+		"type": "note",
+		"title": "HELP",
+		"body": "Shit is hitting Erins fan!",
+		"email": "udit4880@gmail.com"
+	}
+
+	let headers = {
+		'Content-Type': 'application/json'
+	}
+	$.ajax({
+		url: "https://api.pushbullet.com/v2/pushes",
+		data: JSON.stringify(push),
+		type: "POST",
+		beforeSend: function (xhr) { xhr.setRequestHeader('Access-Token', 'o.TWdpN5eMfbFJ6aex5vjmgd9A2qsX6IT').setRequestHeader('Content-Type', 'application/json'); },
+		success: function () { alert("I've been notified darling, will call as soon as I can! Kisses"); }
+	});
+}
+
+
+function cleanName(name) {
+	name = name.replace('.mp3', '')
+	name = name.replace('.aac', '')
+	return name
+}
+
+
+jQuery.fn.visible = function () {
+	return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function () {
+	return this.css('visibility', 'hidden');
+};
+
+jQuery.fn.visibilityToggle = function () {
+	return this.css('visibility', function (i, visibility) {
+		return (visibility == 'visible') ? 'hidden' : 'visible';
+	});
+};
